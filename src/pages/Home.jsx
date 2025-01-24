55288d8fb380b7760d3f43c7ac4f20ed2e9a10c9
ssurasenn <ssurasenn@gmail.com>
2025-01-24 16:00:05 +0700

    deploy

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 //rafce
 import React from "react";
-import ContentCarousel from "../components/home/contentCarousel";
+import ContentCarousel from "../components/home/ContentCarousel";
 import BestSeller from "../components/home/BestSeller";
 import NewProduct from "../components/home/NewProduct";
 
